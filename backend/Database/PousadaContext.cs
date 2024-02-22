cc849b3f2096c3f599588938110c09e9a7cd3e2a
Tiago Queiroz <tiago.dev.queiroz@gmail.com>
2024-02-22 16:35:00 -0300

    Configuraçao do ambiente de desenvolvimento backend

---
+++ backend/Database/PousadaContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Database
+{
+    public class PousadaContext : IdentityDbContext
+    {
+        public PousadaContext(DbContextOptions<PousadaContext> options) : base(options){}
+
+        public DbSet<ClienteModel> clientes { get; set; }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,22 @@
+using backend.Database;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-builder.Services.AddControllers();
-string mySqlConnection = builder.Configuration.GetConnectionString("DefaultDatabase");
+//configuraçao do banco de dados
+var stringconection = builder.Configuration.GetConnectionString("Conexao");
+builder.Services.AddDbContext<PousadaContext>(opt => {opt.UseMySql(stringconection, ServerVersion.AutoDetect(stringconection));});
 
-// Configure the HTTP request pipeline.
+//configuraçao do identity
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<PousadaContext>();
+
+var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +25,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
