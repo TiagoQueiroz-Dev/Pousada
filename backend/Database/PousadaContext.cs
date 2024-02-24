d886458288e68d6156bc04956b1677ee6b4e1de1
Kevin Céssar <102882654+KevinCesarDev@users.noreply.github.com>
2024-02-24 14:50:16 -0300

    ajuste_cliente_model

--- backend/Database/PousadaContext.cs
+++ backend/Database/PousadaContext.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using backend.Models;
+using backend.Entidades.Cliente.Model;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
